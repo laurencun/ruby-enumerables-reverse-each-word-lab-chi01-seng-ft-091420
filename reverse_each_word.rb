40a24c1cb0a94f7f25b6af2119d7cc67446401ff
Lauren Cunningham <lauren.busbee@gmail.com>
2020-07-31 16:12:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ require 'pry'
 
 def reverse_each_word(sentence)
   array = sentence.split()
-  array.collect {|word| word.reverse}
-  sentence.to_s
+  array.collect {|sentence| sentence.to_s}
+  sentence.reverse
   end
